c4695cdc67650c98ca7c450eb22da2f545e3c3ad
DarrenWray <darren@darrenwray.co.uk>
2026-05-22 15:33:42 +0100

    Update Monospire cask to 1.2.2

--- Casks/monospire.rb
+++ Casks/monospire.rb
@@ -1,10 +1,10 @@
 cask "monospire" do
-  version "1.2.1"
-  sha256 "abfc44425ed54cf7d93deff88647f55ba006050eccb9d0a36489e16f2b2a55b8"
+  version "1.2.2"
+  sha256 "1507e65044d3d7bc63f8d614a665348138a6590d61a16cf0ea031e8631027c32"
 
-  url "https://github.com/CurzonMonroe/Monospire/releases/download/v1.2.1/Monospire-1.2.1-arm64.dmg"
+  url "https://github.com/CurzonMonroe/Monospire/releases/download/v1.2.2/Monospire-1.2.2-arm64.dmg"
   name "Monospire"
-  desc "Native-feeling MacOS Markdown editor with dual editing views"
+  desc "Native-feeling macOS Markdown editor with dual editing views"
   homepage "https://github.com/CurzonMonroe/Monospire"
 
   auto_updates true
